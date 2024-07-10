590abb61f03d863aebb8e90d1c607e5ce5bec6c3
TEXTIL\gabriel.sousa <gabriel.sousa@tbmtextil.com.br>
2024-07-10 12:49:14 -0300

    inter font and starting home page

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,11 @@
 export function App() {
   return (
-    <h1 className='text-5xl font-bold underline'>Hello World!</h1>
+    <div>
+      <p>Convide seus amigos e planeje sua próxima viagem!</p>
+      <p>
+      Ao planejar sua viagem pela plann.er você automaticamente concorda
+      com nossos <a href="#">termos de uso</a> e <a href="#">políticas de privacidade</a>.
+      </p>
+    </div>
   )
 }
